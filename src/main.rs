069bab026c356247e578c5c52dcead5ecb8c5397
Sean Geoghegan <seangeo@gmail.com>
2022-05-30 22:26:41 +0930

    Remove use of String

--- src/main.rs
+++ src/main.rs
@@ -4,45 +4,43 @@ use std::error::Error;
 mod hack {
     use std::iter::Iterator;
 
-    pub fn strip_comment(line: &str) -> Option<String> {
-        let line_parts: Vec<&str> = line.split("//").take(1).collect();
-        let line = line_parts.join("");
-        let line = line.trim();
-        let line = line.replace(" ", "");
+    pub fn strip_comment<'a>(line: &'a str) -> Option<&'a str> {
+        let line_parts: Vec<&str>= line.split("//").take(1).collect();
+        let line = line_parts[0].trim();
 
         if line.is_empty() {
             None
         } else {
-            Some(line.to_string())
+            Some(line)
         }
     }
 
-    pub fn to_instruction(line: String) -> Instruction {
+    pub fn to_instruction(line: &str) -> Instruction {
         match line.strip_prefix("@") {
-            Some(address) => Instruction::A(address.to_string()),
+            Some(address) => Instruction::A(address),
             None => Instruction::c_from_string(line)
         }
     }
 
     #[derive(Debug)]
-    pub enum Instruction {
-        A(String),
-        C(String, String, String)
+    pub enum Instruction<'a> {
+        A(&'a str),
+        C(&'a str, &'a str, &'a str)
     }
 
-    impl Instruction {
-        fn parse_comp_and_jump(s: &str) -> (String, String) {
+    impl Instruction<'_> {
+        fn parse_comp_and_jump(s: &str) -> (&str, &str) {
             let comp_and_jump_parts: Vec<&str> = s.split(";").collect();
 
             if comp_and_jump_parts.len() == 1 {
-                (comp_and_jump_parts[0].to_string(), "".to_string())
+                (comp_and_jump_parts[0].trim(), "")
             } else {
-                (comp_and_jump_parts[0].to_string(), comp_and_jump_parts[1].to_string())
+                (comp_and_jump_parts[0].trim(), comp_and_jump_parts[1].trim())
             }
         }
 
-        fn c_from_string(s: String) -> Instruction {
-            let mut dest = String::new();
+        fn c_from_string(s: &str) -> Instruction {
+            let mut dest = "";
             let comp;
             let jump;
 
@@ -51,7 +49,7 @@ mod hack {
             if dest_parts.len() == 1 {
                 (comp, jump) = Self::parse_comp_and_jump(dest_parts[0]);
             } else {
-                dest = String::from(dest_parts[0]);
+                dest = dest_parts[0];
                 (comp, jump) = Self::parse_comp_and_jump(dest_parts[1]);
             }
 
